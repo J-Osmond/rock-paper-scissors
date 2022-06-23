7610a144c1b073d4c4714512bb5a9363eeecef98
James <j.osmond.dev@gmail.com>
2022-06-23 19:36:52 +0100

    Define playRound

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ let computerSelection = computerPlay();
 
 //will randomly select rock, paper or scissors
 function computerPlay() {
-    
+
     let computerOptions = ['rock', 'paper', 'scissors'];
 
     let randomChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
 
     return randomChoice;
+};
+
+function playRound(playerSelection, computerSelection) {
+    
 };
 
