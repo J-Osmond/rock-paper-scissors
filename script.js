12e20165e05c9385b361746ef4d8684d35e01576
James <j.osmond.dev@gmail.com>
2022-06-23 19:32:03 +0100

    Add randomChoice

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let computerSelection = computerPlay();
 
 //will randomly select rock, paper or scissors
 function computerPlay() {
+    
     let computerOptions = ['rock', 'paper', 'scissors'];
+
+    let randomChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+
+    return randomChoice;
 };
 
