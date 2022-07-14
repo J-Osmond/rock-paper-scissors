8b39ec38a745bb7bb20a13037f7e0e3d29952873
James <j.osmond.dev@gmail.com>
2022-07-14 13:55:43 +0100

    Add logic for rock Added code to respond to rock. Moved computerSelection and playerSelection to playRound

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const playerSelection = 'rock';
-let computerSelection = computerPlay();
-
 //will randomly select rock, paper or scissors
 function computerPlay() {
 
-    let computerOptions = ['rock', 'paper', 'scissors'];
+    let computerOptions = ['rock', 'paper','scissors'];
 
     let randomChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
 
@@ -12,6 +9,16 @@ function computerPlay() {
 };
 
 function playRound(playerSelection, computerSelection) {
-    
+
+    computerSelection = computerPlay();
+    playerSelection = 'rock';
+
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'You Lose!';
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'You Win!';
+    } else {
+        return 'Draw!'
+    }
 };
 
