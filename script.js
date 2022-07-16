8449651986ce4860248096e3e2aaa8f19d63d7f0
James <j.osmond.dev@gmail.com>
2022-07-16 16:14:53 +0100

    Add loop to game() playround plays 5 times. Add console.log to outcomes to make sure they work

--- script.js
+++ script.js
@@ -14,31 +14,44 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = prompt('Rock,Paper, Scissors?');
 
     if (playerSelection == 'rock' && computerSelection == 'paper') {
+        console.log('lose')
         return 'You Lose!';
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        console.log('win')
         return 'You Win!';
     } else if (playerSelection == 'rock' && computerSelection == 'rock') {
+        console.log('draw');
         return 'Draw!';
     }
 
     else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        console.log('lose')
         return 'You Lose!';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        console.log('win')
         return 'You Win!';
     } else if ((playerSelection == 'paper' && computerSelection == 'paper')){
+        console.log('draw');
         return 'Draw!'
     }
 
     else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        console.log(lose)
         return 'You Lose!';
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        console.log(win)
         return 'You Win!';
     } else {
+        console.log(draw);
         return 'Draw!'
+        
     };
 };
 
 function game() {
-
+    for (let games = 0; games < 5; games++) {
+        playRound()
+        
+    }
 };
 
