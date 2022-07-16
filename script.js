e9c98e680b5c1518ff8d1f122cbc779a928c8e56
James <j.osmond.dev@gmail.com>
2022-07-16 21:30:11 +0100

    Rework playround to be more readable

--- script.js
+++ script.js
@@ -14,46 +14,28 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
 
     computerSelection = computerPlay();
-    playerSelection = prompt('Rock,Paper, Scissors?').toLowerCase;
+    playerSelection = prompt('Rock,Paper, Scissors?');
 
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log('lose')
-        return 'You Lose!';
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        console.log('win')
-        return 'You Win!';
-    } else if (playerSelection == 'rock' && computerSelection == 'rock') {
-        console.log('draw');
-        return 'Draw!';
-    }
-
-    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        console.log('lose')
-        return 'You Lose!';
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log('win')
-        return 'You Win!';
-    } else if ((playerSelection == 'paper' && computerSelection == 'paper')){
-        console.log('draw');
-        return 'Draw!'
-    }
-
-    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        console.log('lose')
-        return 'You Lose!';
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        console.log('win')
-        return 'You Win!';
-    } else {
-        console.log('draw');
-        return 'Draw!'
+    if (playerSelection == 'rock' && computerSelection == 'paper' ||
+        playerSelection == 'paper' && computerSelection == 'scissors' ||
+        playerSelection == 'scissors' && computerSelection == 'rock') {
+            console.log('lose');
+        } 
         
-    };
+        else if (playerSelection == 'rock' && computerSelection == 'scissors' ||
+                playerSelection == 'paper' && computerSelection == 'rock' ||
+                playerSelection == 'scissors' && computerSelection == 'paper') {
+                    console.log('win');
+                } 
+                
+                else {
+                    return;
+                };
+
 };
 
 function game() {
     for (let games = 0; games < 5; games++) {
         playRound()
-        
     }
 };
